de9bda2e72dc8e5922fa90941370f4cee885475f
skaricheri <sruthimanoj@gmail.com>
2020-01-17 13:51:56 -0600

    initial code with basic displays and filters

--- ET-level-1/static/js/app.js
+++ ET-level-1/static/js/app.js
@@ -1,4 +1,59 @@
-// from data.js
-var tableData = data;
 
-// YOUR CODE HERE!
+// Select the table body
+var tableBody=d3.select("tbody");
+
+// Data from data.js
+renderTable(data);
+
+// Form element selection
+// Input Box and Button 
+var filterButton = d3.select("#filter-btn");
+var inputBox = d3.select(".form-control");
+
+
+// Button event click and call back function
+filterButton.on('click', click => filterTable());
+    
+        
+   
+
+// Function Button Event check
+function filterTable(){
+    var inputDate = inputBox.property('value');
+
+    if (inputDate === "" ){
+         
+         window.alert("Please enter a date to filter the sighting !!");
+         renderTable(data);
+         console.log("table loaded");
+        }
+    else {
+    // console.log(inputDate);
+
+    // Filter the data by the user input
+    var filteredData= data.filter(event => (event.datetime === inputDate));
+    // console.log(filteredData);
+    // Call back function to rerenders Table with filtered data 
+    renderTable(filteredData);
+};
+};
+
+// Table rendering call back function
+function renderTable(table) {
+    // clears table before rendering again 
+    tableBody.html('');
+    
+    table.forEach(sighting => {
+        // add new row to table
+    var row = tableBody.append('tr');
+
+    Object.values(sighting).forEach(details => row.append('td').text(details));
+      
+    
+    });
+};
+
+    
+    
+
+    
